d8c7323666f71357bcd8caf5dac30c012ba29e71
01-Destiny <emoakpordestiny@gmail.com>
2023-03-17 05:22:59 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char m;
+
+	for (m = 'a'; m <= 'z'; m++)
+		putchar(m);
+	putchar('\n');
+	return (0);
+}
