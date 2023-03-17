42315ce05d68a5bc7b0901560c286f91fbd16725
01-Destiny <emoakpordestiny@gmail.com>
2023-03-17 06:04:14 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int d;
+
+	for (d = 0; d < 10; d++)
+		printf("%d", d);
+	printf("\n");
+	return (0);
+}
