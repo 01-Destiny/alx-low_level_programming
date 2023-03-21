9db01f717d0e45bce2d0c56906ffb5d3940c0806
01-Destiny <emoakpordestiny@gmail.com>
2023-03-21 10:47:59 -0700

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints natural numbers from n to 98
+ *
+ * @n: function parameter
+ *
+ * Return: always 0
+ */
+
+void print_to_98(int n)
+{
+	if (n > 98)
+	{
+		for (; n > 98; n--)
+		{
+			printf("%d, ", n);
+		}
+	}
+	else if (n < 98)
+	{
+		for (; n < 98; n++)
+		{
+			printf("%d, ", n);
+		}
+	}
+	printf("%d\n", n);
+}
