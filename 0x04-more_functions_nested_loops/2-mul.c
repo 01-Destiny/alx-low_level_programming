cce70882a1517dabef5b1c6900e7e4817b2e72d6
01-Destiny <emoakpordestiny@gmail.com>
2023-03-23 05:05:14 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ *
+ * @a: first parameter
+ *
+ * @b: second parameter
+ *
+ * Return: always 0
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
