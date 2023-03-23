549a91d8886fddd8fe95b9de2f4cb798ccbd8af7
01-Destiny <emoakpordestiny@gmail.com>
2023-03-23 04:53:24 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for a digit
+ *
+ * @c: parameter
+ *
+ * Return: 1 if c is a digit, otherwise 0
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
