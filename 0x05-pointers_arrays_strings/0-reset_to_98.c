653b475de95fb6fddf063c0a33e113af8d291c0c
01-Destiny <emoakpordestiny@gmail.com>
2023-03-27 01:59:29 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer to an int and updates the value
+ *
+ * @n: function parameter
+ *
+ * Return: no return
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
