fc5dd5107f502431103e629f52d63fc8ab7dbd89
01-Destiny <emoakpordestiny@gmail.com>
2023-03-29 11:54:53 -0700

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *_strcmp -  compares two strings
+ *@s1: A pointer to an char that will be updated
+ *@s2: A pointer to an char that will be updated
+ *Return: dest
+ */
+
+int _strcmp(char *s1, char *s2)
+
+{
+
+	char *str_one = s1;
+	char *str_two = s2;
+
+	while (*str_one != '\0' && *str_two != '\0' &&  *str_one == *str_two)
+	{
+		str_one++;
+		str_two++;
+	}
+
+	return (*str_one - *str_two);
+}
