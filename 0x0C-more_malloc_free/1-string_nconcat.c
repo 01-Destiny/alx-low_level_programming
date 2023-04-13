b6cdc1089adda90be9bd107b537f899899230a73
01-Destiny <emoakpordestiny@gmail.com>
2023-04-13 08:55:39 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,41 +2,48 @@
 #include <stdlib.h>
 
 /**
- * string_nconcat - concatenates two strings
- *
- * @s1: string 1
- *
- * @s2: string 2
- *
- * @n: number of bytes
+ * string_nconcat - concatenates two strings.
+ * @s1: first string.
+ * @s2: second string.
+ * @n: amount of bytes.
  *
  * Return: not always 0
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *p;
-	unsigned int i1, i2, i3, i;
+	char *sout;
+	unsigned int ls1, ls2, lsout, i;
 
 	if (s1 == NULL)
-		s1 = " ";
+		s1 = "";
+
 	if (s2 == NULL)
-		s2 = " ";
-	for (i1 = 0; s1[i1] != '\0'; i1++)
+		s2 = "";
+
+	for (ls1 = 0; s1[ls1] != '\0'; ls1++)
 		;
-	for (i2 = 0; s2[i2] != '\0'; i2++)
+
+	for (ls2 = 0; s2[ls2] != '\0'; ls2++)
 		;
-	if (n > i2)
-		n = i2;
-	i3 = i1 + n;
-	p = malloc(i3 + 1);
-	if (p == NULL)
+
+	if (n > ls2)
+		n = ls2;
+
+	lsout = ls1 + n;
+
+	sout = malloc(lsout + 1);
+
+	if (sout == NULL)
 		return (NULL);
-	for (i = 0; i < i3; i++)
-		if (i < i1)
-			p[i] = s1[i];
+
+	for (i = 0; i < lsout; i++)
+		if (i < ls1)
+			sout[i] = s1[i];
 		else
-			p[i] = s2[i - i1];
-	p[i] = '\0';
-	return (p);
+			sout[i] = s2[i - ls1];
+
+	sout[i] = '\0';
+
+	return (sout);
 }
