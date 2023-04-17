88d6f0e5bd910e6b8bd45fef117249ed99fe861a
01-Destiny <emoakpordestiny@gmail.com>
2023-04-17 06:41:41 -0700

    1-init_dog.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,25 @@
+#include "dog.h"
+
+/**
+ * init_dog - initialize a variable
+ *
+ * @d: struct
+ *
+ * @name: dog name
+ *
+ * @age: dog age
+ *
+ * @owner: dog owner
+ *
+ * Return: no return
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
